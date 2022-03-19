c4510d89d9d503c3993c96ba9e8fd72458385b21
Fadhilrizky01 <Fadhilrizky231@gmail.com>
2022-03-19 22:35:20 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
+import Search from "./components/Search";
 
-function App() {
+const App = () => {
+  // fetch data from API
+  const [images, setImages] = useState([]); // catch data from API
+  const [isLoading, setIsLoading] = useState(true); // loading
+  const [input, setInput] = useState(""); // buat Search
+
+  useEffect(() => {
+    axios
+      .get(`https://pixabay.com/api/?key=26218912-4ed2a2a74309b088b982a280c&q=${input}&image_type=photo`)
+      .then((res) => {
+        setImages(res.data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [input]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto sm:ml-4">
+      <Search searchText={(text) => setInput(text)} />
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images found</h1>}
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading....</h1>
+      ) : (
+        <div className="flex flex-wrap justify-center items-center gap-4">
+          {images.map((image) => {
+            return <Card key={image.id} image={image} />;
+          })}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
